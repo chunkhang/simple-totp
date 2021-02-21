49b5d060cfc97ea06f59298e99e1a3c236b3c9cb
Marcus Mu <chunkhang@gmail.com>
2021-02-21 23:14:17 +0800

    Refactor totp columns

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from contextlib import contextmanager
 from datetime import datetime
 import math
 import os
@@ -8,8 +7,11 @@ import time
 import yaml
 
 CONFIG_FILENAME = '.totp.yml'
+DEFAULT_TOTP_ISSUER = '-'
+DEFAULT_TOTP_NAME = '-'
 DEFAULT_TOTP_INTERVAL = 30
 DEFAULT_TOTP_DIGITS = 6
+COLUMN_SEPARATOR = '|'
 
 
 def main():
@@ -26,39 +28,60 @@ def main():
                 s=document['secret'],
                 interval=document['interval'] if 'interval' in document else DEFAULT_TOTP_INTERVAL,
                 digits=document['digits'] if 'digits' in document else DEFAULT_TOTP_DIGITS,
-                issuer=document['issuer'],
-                name=document['name'],
+                issuer=document['issuer'] if 'issuer' in document else DEFAULT_TOTP_ISSUER,
+                name=document['name'] if 'name' in document else DEFAULT_TOTP_NAME,
                 )
         totps.append(totp)
 
-    # Calculate max width for each column
-    identifier_width = max(map(lambda totp: len(get_totp_identifier(totp)), totps))
-    code_width = max(map(lambda totp: totp.digits, totps))
-    seconds_width = max(map(lambda totp: len(str(totp.interval)), totps))
+    # Getter functions to calculate column value and width
+    # Each totp row will have these columns
+    column_getters = [
+            # Issuer
+            {
+                'value': lambda totp: totp.issuer,
+                'width': lambda totp: len(totp.issuer),
+                },
+            # Name
+            {
+                'value': lambda totp: totp.name,
+                'width': lambda totp: len(totp.name),
+                },
+            # Seconds left
+            {
+                'value': lambda totp: str(math.trunc(totp.interval - (datetime.now().timestamp() % totp.interval))),
+                'width': lambda totp: len(str(totp.interval)),
+                },
+            # Code
+            {
+                'value': lambda totp: totp.now(),
+                'width': lambda totp: totp.digits,
+                },
+            ]
+
+    # Calculate maximum width for all columns
+    column_widths = []
+    for getter in column_getters:
+        getter['width'](totp)
+        widths = map(lambda totp: getter['width'](totp), totps)
+        column_widths.append(max(widths))
 
     try:
         while True:
-            for i, totp in enumerate(totps):
-                seconds_left = math.trunc(totp.interval - (datetime.now().timestamp() % totp.interval))
-                row = '{} | {} | {}'.format(
-                        get_totp_identifier(totp).ljust(identifier_width),
-                        str(seconds_left).rjust(seconds_width),
-                        totp.now().ljust(code_width),
-                        )
-                sys.stdout.write(f'{row}\n')
+            for totp in totps:
+                values = map(lambda getter: getter['value'](totp), column_getters)
+                columns = []
+                for i, value in enumerate(values):
+                    column = value.ljust(column_widths[i])
+                    columns.append(column)
+                row = f' {COLUMN_SEPARATOR} '.join(columns)
+                sys.stdout.write(row)
+                sys.stdout.write('\n')
             sys.stdout.flush()
             # Move cursor up until we reach the top
             sys.stdout.write('\033[F' * len(totps))
             time.sleep(1)
     except KeyboardInterrupt:
         pass
-
-
-def get_totp_identifier(totp):
-    return '{}:{}'.format(
-            totp.issuer,
-            totp.name
-            )
 
 
 if __name__ == '__main__':
